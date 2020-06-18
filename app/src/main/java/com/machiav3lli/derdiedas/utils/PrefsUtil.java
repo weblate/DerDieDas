fcf7685e482da88d85eaffb6b9924c2bfb2cbd26
machiav3lli <https://machiav3lli@github.com>
2020-06-18 15:42:08 +0200

    Update: move to private Prefs

--- app/src/main/java/com/machiav3lli/derdiedas/utils/PrefsUtil.java
+++ app/src/main/java/com/machiav3lli/derdiedas/utils/PrefsUtil.java
@@ -3,12 +3,12 @@ package com.machiav3lli.derdiedas.utils;
 
 import android.content.Context;
 
-import androidx.preference.PreferenceManager;
+import com.machiav3lli.derdiedas.Constants;
 
 public class PrefsUtil {
 
     public static String getPrefsString(Context context, String key, String def) {
-        return PreferenceManager.getDefaultSharedPreferences(context).getString(key, def);
+        return context.getSharedPreferences(Constants.PREFS_SHARED, Context.MODE_PRIVATE).getString(key, def);
     }
 
     public static String getPrefsString(Context context, String key) {
@@ -16,6 +16,6 @@ public class PrefsUtil {
     }
 
     public static void setPrefsString(Context context, String key, String value) {
-        PreferenceManager.getDefaultSharedPreferences(context).edit().putString(key, value).apply();
+        context.getSharedPreferences(Constants.PREFS_SHARED, Context.MODE_PRIVATE).edit().putString(key, value).apply();
     }
 }
