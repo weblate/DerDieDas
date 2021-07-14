54c0e5ac04b4181dd419dc9f47930d421c86ab3d
machiav3lli <antonios.hazim@tutanota.com>
2021-07-14 02:00:01 +0200

    Update: Migrate StatsActivity to Kotlin

---
+++ app/src/main/java/com/machiav3lli/derdiedas/ui/StatsActivity.kt
@@ -0,0 +1,51 @@
+package com.machiav3lli.derdiedas.ui
+
+import android.os.Bundle
+import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import androidx.appcompat.app.AppCompatActivity
+import com.machiav3lli.derdiedas.R
+import com.machiav3lli.derdiedas.databinding.ActivityStatsBinding
+import com.machiav3lli.derdiedas.utils.DatabaseUtil
+import com.machiav3lli.derdiedas.utils.FileUtils.getNounsCount
+import java.io.UnsupportedEncodingException
+import java.util.*
+
+class StatsActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityStatsBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityStatsBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+        try {
+            setWordStats()
+        } catch (e: UnsupportedEncodingException) {
+            e.printStackTrace()
+        }
+        binding.back.setOnClickListener { onBackPressed() }
+        binding.statsInfo.setOnClickListener { onFullWords() }
+    }
+
+    @Throws(UnsupportedEncodingException::class)
+    private fun setWordStats() {
+        val allNouns = getNounsCount(this)
+        Thread {
+            val remainingNouns = DatabaseUtil(this).nounsCount
+            val learnedWords = allNouns - remainingNouns
+            findViewById<TextView>(R.id.word_stats).text =
+                String.format(Locale.ENGLISH, "%d / %d", learnedWords, allNouns)
+        }.start()
+    }
+
+    private fun onFullWords() {
+        val titleString = resources.getString(R.string.full_words_title)
+        val textString = resources.getString(R.string.full_words_text)
+        val builder = AlertDialog.Builder(this, R.style.AlertDialogCustom)
+        builder.setTitle(titleString)
+        builder.setMessage(textString)
+        builder.setPositiveButton("OK", null)
+        val dialog = builder.create()
+        dialog.show()
+    }
+}
