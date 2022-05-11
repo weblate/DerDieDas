b0ea1bdddc59fa6ff25b94060dd71587cc210021
machiav3lli <antonios.hazim@tutanota.com>
2022-05-12 00:51:28 +0200

    Update: Migrate build scripts to Kotlin-DSL

---
+++ build.gradle.kts
@@ -0,0 +1,21 @@
+buildscript {
+    repositories {
+        mavenCentral()
+        google()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:7.2.0")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:1.6.21")
+    }
+}
+
+allprojects {
+    repositories {
+        mavenCentral()
+        google()
+    }
+}
+
+tasks.register("clean", Delete::class) {
+    delete(rootProject.buildDir)
+}
