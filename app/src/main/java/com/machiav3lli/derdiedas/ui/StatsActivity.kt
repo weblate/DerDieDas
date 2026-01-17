6119f8a3a504357125057170f79773c1942bbef0
machiav3lli <antonios.hazim@tutanota.com>
2026-01-17 23:33:59 +0100

    Update: Replace Stats layout with composable

--- app/src/main/java/com/machiav3lli/derdiedas/ui/StatsActivity.kt
+++ app/src/main/java/com/machiav3lli/derdiedas/ui/StatsActivity.kt
@@ -1,53 +1,21 @@
 package com.machiav3lli.derdiedas.ui
 
 import android.os.Bundle
-import android.widget.TextView
-import androidx.appcompat.app.AlertDialog
-import com.machiav3lli.derdiedas.R
-import com.machiav3lli.derdiedas.databinding.ActivityStatsBinding
-import com.machiav3lli.derdiedas.utils.getNounsCount
-import kotlinx.coroutines.runBlocking
-import java.io.UnsupportedEncodingException
-import java.util.Locale
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import com.machiav3lli.derdiedas.ui.theme.AppTheme
 
 class StatsActivity : BaseActivity() {
-    private lateinit var binding: ActivityStatsBinding
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityStatsBinding.inflate(layoutInflater)
-        setContentView(binding.root)
-        try {
-            setWordStats()
-        } catch (e: UnsupportedEncodingException) {
-            e.printStackTrace()
-        }
-        binding.back.setOnClickListener { onBackPressed() }
-        binding.statsInfo.setOnClickListener { onFullWords() }
-    }
 
-    // TODO replace within the Composable screen
-    @Throws(UnsupportedEncodingException::class)
-    private fun setWordStats() {
-        Thread {
-            runBlocking {
-                val (learnedWords, allNouns) = this@StatsActivity.getNounsCount()
-                runOnUiThread {
-                    findViewById<TextView>(R.id.word_stats).text =
-                        String.format(Locale.ENGLISH, "%d / %d", learnedWords, allNouns)
-                }
+        setContent {
+            AppTheme {
+                StatsScreen(
+                    onBackPressed = { finish() }
+                )
             }
-        }.start()
-    }
-
-    private fun onFullWords() {
-        val titleString = resources.getString(R.string.full_words_title)
-        val textString = resources.getString(R.string.full_words_text)
-        val builder = AlertDialog.Builder(this, R.style.AlertDialogCustom)
-        builder.setTitle(titleString)
-        builder.setMessage(textString)
-        builder.setPositiveButton("OK", null)
-        val dialog = builder.create()
-        dialog.show()
+        }
     }
 }
