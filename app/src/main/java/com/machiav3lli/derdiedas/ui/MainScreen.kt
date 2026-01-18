ae34928a6ddba7c4aa84b320684481a0c26f8089
machiav3lli <antonios.hazim@tutanota.com>
2026-01-18 16:59:43 +0100

    Fix: Theme handling of background and system bars

--- app/src/main/java/com/machiav3lli/derdiedas/ui/MainScreen.kt
+++ app/src/main/java/com/machiav3lli/derdiedas/ui/MainScreen.kt
@@ -9,7 +9,6 @@ import androidx.compose.material3.Scaffold
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
 import com.machiav3lli.derdiedas.R
@@ -18,7 +17,7 @@ import com.machiav3lli.derdiedas.ui.navigation.NavRoute
 @Composable
 fun MainScreen(navigator: (NavRoute) -> Unit) {
     Scaffold(
-        containerColor = Color.Transparent,
+        containerColor = MaterialTheme.colorScheme.background,
         contentColor = MaterialTheme.colorScheme.onBackground,
     ) { paddingValues ->
         Column(
--- app/src/main/java/com/machiav3lli/derdiedas/ui/theme/Theme.kt
+++ app/src/main/java/com/machiav3lli/derdiedas/ui/theme/Theme.kt
@@ -43,8 +43,11 @@ fun isNightMode(): Boolean = when (AppCompatDelegate.getDefaultNightMode()) {
     else -> isSystemInDarkTheme()
 }
 
-fun Context.isNightMode() =
-    resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+fun Context.isNightMode() = when (AppCompatDelegate.getDefaultNightMode()) {
+    AppCompatDelegate.MODE_NIGHT_YES -> true
+    AppCompatDelegate.MODE_NIGHT_NO -> false
+    else -> resources?.configuration?.uiMode?.and(Configuration.UI_MODE_NIGHT_MASK) == Configuration.UI_MODE_NIGHT_YES
+}
 
 private val lightScheme = lightColorScheme(
     primary = primaryLight,
